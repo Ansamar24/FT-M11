fc8c176eeeee34478dcda884e15c6ec48984419a
Ansamar24 <andrea24094@gmail.com>
2023-08-16 11:49:17 -0500

    homework1

--- FT-M1/01-IntroToCS/homework/homework.js
+++ FT-M1/01-IntroToCS/homework/homework.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   var resultado = 0 
-   hhsaaahaaj
+   var suma = 0 
+   var exponente = num.length -1
+
+   for(let i=0; i < num.lenght; i++){
+      suma = suma + num[i] * Math.pow(2,exponente);
+      exponente--;
+   }
+   return suma 
 }
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   var resultado=""
+
+   while(num>0){
+      resultado = (num%2) + resultado;
+      num = Math.floor(num / 2)
+   }
+}
 
 module.exports = {
    BinarioADecimal,
